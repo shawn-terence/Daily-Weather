1ccf0daf4e90981fdbcdf60b2bcb0132aaf539df
terence <terenceshawn18@outlook.com>
2024-07-17 13:32:29 +0300

    init:setting up files

---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+import {siteConfig } from "@/config/site";
+import { useEffect, useState } from "react";
+import {Card, CardHeader, CardBody,Divider,Image,Spacer} from "@nextui-org/react";
+import "../styles/App.css"
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
+import WeatherDisplay from "@/components/ForeCast";
+import BarChartF from "@/components/BarChart";
