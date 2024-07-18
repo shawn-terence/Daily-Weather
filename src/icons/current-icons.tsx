00d7c884af93f1e2fcddc91322c7eb9746e3ce92
terence <terenceshawn18@outlook.com>
2024-07-18 13:00:21 +0300

    feat:Added location option and icons

---
+++ src/icons/current-icons.tsx
@@ -0,0 +1,22 @@
+
+export const Icons ={
+    'sunny':"https://www.iconarchive.com/download/i18088/icons-land/weather/Sunny.ico",
+    'cloudy':"https://cdn3.iconfinder.com/data/icons/nature-emoji/50/Cloudy-512.png",
+    'rain':"https://cdn0.iconfinder.com/data/icons/spring-187/512/raining_rain_cloud_weather-512.png",
+    'snowy':"https://cdn1.iconfinder.com/data/icons/line-christmas-icons/75/_snowflake-512.png",
+    'partly cloudy':"https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-02-512.png",
+    'clear':"https://www.iconarchive.com/download/i18088/icons-land/weather/Sunny.ico",
+    'patchy rain nearby':"https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-03-512.png",
+    'light rain':"https://cdn4.iconfinder.com/data/icons/mini-material-design-weather-icons-multi-color/32/rain_light-512.png",
+    'moderate rain':"https://cdn2.iconfinder.com/data/icons/weather-1070/64/weather_moderate_rainfall-512.png",
+    'heavy rain':"https://cdn4.iconfinder.com/data/icons/doodle-5/154/rain-heavy-512.png",
+    'light snow':"https://cdn1.iconfinder.com/data/icons/weather-forecast-meteorology-color-1/128/weather-snow-light-512.png",
+    'moderate snow':"https://cdn0.iconfinder.com/data/icons/winter-171/128/snowfall-512.png",
+    'heavy snow':"https://cdn0.iconfinder.com/data/icons/winter-171/128/snowfall-512.png",
+    'smoke':"https://cdn1.iconfinder.com/data/icons/world-pollution-3/512/air-pollutiom-dust-smoke-pm2.5-environment-512.png",
+    'haze':"https://cdn3.iconfinder.com/data/icons/aami-web-internet/64/aami9-83-512.png",
+    'mist':"https://cdn2.iconfinder.com/data/icons/buno-weather/32/breeze_fog_haze_mist_weather_wind-512.png",
+    'fog':"https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_30-512.png",
+    'smog':"https://cdn2.iconfinder.com/data/icons/pm2-5-air-pollution-1/512/pm2.5-air-pollution-dust-20-512.png",
+    "light rain shower":"https://cdn4.iconfinder.com/data/icons/mini-material-design-weather-icons-multi-color/32/rain_light-512.png"
+}
